4a5647644c6c2ec11fd24f7d22c4d503aed6c204
Christoffer Björnram <christofferbjornram@hotmail.com>
2025-10-18 23:50:19 +0200

    Initial Commit

---
+++ HomeSpanSunSimulator.ino
@@ -0,0 +1,203 @@
+
+#define NEOPIXEL_RGB_PIN  27                    // Set pin for LED Strip.
+#define TIME_ZONE         "UTC-2:00"            // Set time zone according to the Proleptic Format for TZ. See https://sourceware.org/glibc/manual/latest/html_node/Proleptic-TZ.html for examples.
+#define NTP_SERVER        "192.168.178.2"       // Optional: Set desired NTP server.
+ 
+#include "HomeSpan.h"
+#include <WebServer.h>
+
+///////////////////////////////
+
+struct NeoPixel_RGB : Service::LightBulb {      // Addressable single-wire RGB LED Strand (e.g. NeoPixel)
+ 
+  Characteristic::On power{0,true};
+  Characteristic::Hue H{0,true};
+  Characteristic::Saturation S{0,true};
+  Characteristic::Brightness V{100,true};
+  Pixel *pixel;
+  int nPixels;
+  
+  NeoPixel_RGB(uint8_t pin, int nPixels) : Service::LightBulb(){
+    V.setRange(5,100,1);                      // sets the range of the Brightness to be from a min of 5%, to a max of 100%, in steps of 1%
+    pixel=new Pixel(pin);                     // creates Pixel RGB LED on specified pin
+    this->nPixels=nPixels;                    // save number of Pixels in this LED Strand
+    update();                                 // manually call update() to set pixel with restored initial values
+  }
+
+  boolean update() override {
+
+    int p=power.getNewVal();
+    
+    float h=H.getNewVal<float>();       // range = [0,360]
+    float s=S.getNewVal<float>();       // range = [0,100]
+    float v=V.getNewVal<float>();       // range = [0,100]
+
+    Pixel::Color color;
+
+    pixel->set(color.HSV(h*p, s*p, v*p),nPixels);       // sets all nPixels to the same HSV color
+          
+    return(true);
+  }
+};
+
+struct DEV_LED : Service::LightBulb {
+
+  SpanCharacteristic *power;
+  int alarmHour;
+  int alarmMinute;
+  
+  DEV_LED(int alarmHour, int alarmMinute) : Service::LightBulb(){
+    power=new Characteristic::On();
+    this->alarmHour=alarmHour;
+    this->alarmMinute=alarmMinute;
+  }
+
+  boolean update(){            
+    return(true);  
+  }
+};
+
+///////////////////////////////
+
+NeoPixel_RGB *leds;
+DEV_LED *sunSimulation;
+WebServer webServer(80);
+//int alarmHour = 07;
+//int alarmMinute = 00;
+int lastUpdatedSimulationTime = 0;
+
+void setup() {
+  
+  Serial.begin(115200);
+
+  homeSpan.setHostNameSuffix("");
+  homeSpan.setPortNum(1201);
+  homeSpan.setConnectionCallback(setupWeb);
+  homeSpan.enableWebLog(0,NTP_SERVER,TIME_ZONE,NULL);  
+  homeSpan.begin(Category::Lighting,"Pixel Sun");
+
+  new SpanAccessory();
+    new Service::AccessoryInformation();
+      new Characteristic::Name("Pixel LEDS");
+      new Characteristic::Identify();
+    leds=new NeoPixel_RGB(NEOPIXEL_RGB_PIN,300);                       // create 8-LED NeoPixel RGB Strand with full color control
+
+  new SpanAccessory();
+    new Service::AccessoryInformation();
+      new Characteristic::Name("Sun Simulation");
+      new Characteristic::Identify();
+    sunSimulation=new DEV_LED(7, 0);
+
+  new SpanUserCommand('T',"- show current time",printTime);
+}
+
+///////////////////////////////
+
+void loop() {
+  homeSpan.poll();
+  webServer.handleClient();
+  runSunSimulation();
+}
+
+void printTime(const char *buf) {
+  struct tm myTime;        // create a tm structure
+  getLocalTime(&myTime);   // populate the tm structure with current date and time
+  Serial.printf("Current Time = %02d:%02d:%02d\n", myTime.tm_hour, myTime.tm_min, myTime.tm_sec);
+}
+
+///////////////////////////////
+
+void setupWeb(int count){
+
+  if(count>1)
+    return;
+  
+  Serial.printf("Starting Web Server");
+  webServer.begin();
+
+  webServer.on("/", []() {
+    
+    String response = "<html><head><title>Sun Simulation</title>";
+    response += "<body></body></html>";
+    webServer.send(200, "text/html", response);
+
+  });
+
+  // Set alarm time. Expected format is "[IP Address]/set?time=07:30"
+  webServer.on("/set", []() {
+
+    //int time=atoi(webServer.arg(0).c_str());
+    String time = webServer.arg(0).c_str();
+    String hour, minute;
+    hour = time.substring(0, 2);
+    minute = time.substring(3, 5);
+    sunSimulation->alarmHour = hour.toInt();
+    sunSimulation->alarmMinute = minute.toInt();
+
+    String response = "<html>";
+    response += "Sun simulation is set to ";
+
+    sunSimulation->power->setVal(true);
+    
+    response += String(sunSimulation->alarmHour);
+    response += ":";
+    response += String(sunSimulation->alarmMinute);
+    response += ".</html>";
+
+    webServer.send(200, "text/html", response);
+
+  });
+
+  // Request current simulation state
+  webServer.on("/state", []() {
+
+    int index=atoi(webServer.arg(0).c_str());
+
+    String response = "<html><head><title>Sun Simulation</title><meta http-equiv='refresh' content = '3; url=/'/></head>";
+    response += "<body>Sun simulation is: ";
+    response += String(sunSimulation->power->getVal());
+    response += ". Alarm set to: ";
+    response += String(sunSimulation->alarmHour);
+    response += ":";
+    response += String(sunSimulation->alarmMinute);
+    response += ".</body></html>";
+
+    webServer.send(200, "text/html", response);
+
+  });
+}
+
+void runSunSimulation() {
+
+  // If sun simulation is enabled
+  if(sunSimulation->power->getVal()){
+
+    // Save current time and alarm time as minute ints
+    struct tm currentTime;
+    getLocalTime(&currentTime);
+
+    int alarmInMinutes = sunSimulation->alarmHour*60 + sunSimulation->alarmMinute;
+    int currentTimeInMinutes = currentTime.tm_hour*60 + currentTime.tm_min;
+
+    // If within simulation window, run simulation
+    if(alarmInMinutes - currentTimeInMinutes <= 30 && currentTimeInMinutes > lastUpdatedSimulationTime && alarmInMinutes >= currentTimeInMinutes){
+      if(leds->power.getVal() == false){
+        leds->H.setVal(30);
+        leds->power.setVal(true);
+      }
+
+      // Set brightness. 30 (+5) = full sun, 5 = minimum allowed brightness.
+      leds->V.setVal(30-(alarmInMinutes-currentTimeInMinutes)+5);
+      // Set saturation. 80 = goal saturation, 90 = start saturation.
+      leds->S.setVal(80+(alarmInMinutes-currentTimeInMinutes)/3);
+
+      leds->update();
+      lastUpdatedSimulationTime = currentTimeInMinutes;
+    }
+
+    if(alarmInMinutes == currentTimeInMinutes){
+      sunSimulation->power->setVal(false);
+      sunSimulation->update();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# HomeSpan Sun Simulator Wake Up Light
+
+This project uses an ESP32, NeoPixels (or similar addressable LED strips), and HomeSpan to create a HomeKit enabled sun simulation wake up light. In Home, two accessories are added where the first one controls the LED strip as any other RGB enabled light, and the other is an on/off switch if the sun simulation should be active or not. When the sun simulation switch is toggled, nothing visible happens to the LED strip until half an hour before the set alarm time.
+
+## Setting an Alarm
+
+Since HomeKit does not have an easy to use way of slowly dimming a light on over the course of half an hour, this is handled by the ESP32. Due to this, the desired alarm time must be set in the ESP32 using other means. In this project, the ESP32 accepts HTTP GET requests with the alarm time as a parameter in the URL, for example visiting the following URL (with the IP address of the ESP32) would the alarm to 07:30, with the sun simulation starting at 07:00:
+
+```
+192.168.1.32/set?time=07:30
+```
+
+An iOS Shortcut has been created to simplify the process. When the shortcut is run, it will ask for a desired wake up time and then set both an alarm in the iOS alarm app and send an HTTP GET request to the ESP32. The shortcut will soon be linked to from this README page.
+
+Even if the ESP32 is doing the sun simulation by itself, all LED strip values are being reported back to Home so the Home app always show the current state of the accessory even when the simulation is running. When the simulation is done (at the set alarm time), the Sun Simulation accessory is automatically turned off.
+
+## Running the project
+
+This project is based on the great [HomeSpan](https://github.com/HomeSpan/HomeSpan) library and would not have been possible without it.
+
+Clone or download this project, run Arduino IDE, and install the ESP32 boards by Espressif and the HomeSpan library by Gregg Berman. Set the pin your LED strip is connected to, and set your time zone according to the Proleptic Format for TZ (see [https://sourceware.org/glibc/manual/latest/html_node/Proleptic-TZ.html](https://sourceware.org/glibc/manual/latest/html_node/Proleptic-TZ.html) for examples).
+
+## Known Limitations
+
+- In the current version, alarms set between 00:00 and 00:30 will never trigger the sun simulation.
+- The strip can be controlled manually while the simulation is running, however any manual changes will be overridden every new minute for as long as the simulation is running (until the alarm time). The simulation can be disabled at any time by toggling off the "Sun Simulation" accessory in the Home app.
+- There is currently no verification if the entered alarm time is a real time. For example if the value "25:73" is entered as the desired alarm time via URL, the system will fail. So for the moment keep to real time values when using URL. If using the Shortcut to set time, this becomes a non-issue as the alarm time is selected from a time picker.
+
+## Further Development
+
+As this was developed as a simple home project, no further improvements or changes are planned or committed to. Feel free to download and modify to your own needs.
